6e96b077cd64d90961d0d8cec59da7fbbfa365e9
Elad <eladv@mail.tau.ac.il>
2022-12-11 16:08:35 +0200

    added BF method to AVLNode completed insert method added fix_the_tree, perform_rotation, left_rotation, right_rotation, fix_nodes_height_and_size, fix_nodes_size

--- avl_template_new.py
+++ avl_template_new.py
@@ -20,7 +20,7 @@ class AVLNode(object):
         self.left = None
         self.right = None
         self.parent = None
-        self.height = -1
+        self.height = 0
         self.isReal = False if value is None else True
         self.size = 1 if self.isReal else 0
 
@@ -67,6 +67,9 @@ class AVLNode(object):
 
     def getHeight(self):
         return self.height if self.isReal else -1
+
+    def getBalanceFactor(self):
+        return self.getLeft().getHeight() - self.getRight().getHeight()
 
     """returns the size
 
@@ -177,6 +180,7 @@ class AVLTreeList(object):
     @rtype: AVLNode
     @returns: the i'th item in the list
     """
+
     def retrieve_node(self, i):
         def retrieve_rec(node, k):
             r = node.left.size + 1
@@ -203,10 +207,21 @@ class AVLTreeList(object):
             return self.retrieve_node(i).value
         return None
 
-    def get_predecessor(self, node):
+    """returns the predecessor of node in the list
+
+            @type node: AVLNode
+            @param node: node in the list
+            @pre: retrieve(node) != None
+            @rtype: AVLNode
+            @returns: the predecessor of node in the list
+            """
+
+    @staticmethod
+    def get_predecessor(node):
         if node.getLeft() is not None:
             node_tree = AVLTreeList()
-            node_tree.insert(0, node.getLeft())
+            node_tree.root = node.getLeft()
+            node_tree.size = node_tree.root.size
             return node_tree.get_max()
         parent_node = node.getParent()
         while parent_node is not None and node == parent_node.getLeft():
@@ -235,6 +250,7 @@ class AVLTreeList(object):
            @rtype: AVLNode
            @returns: the last item, None if the list is empty
            """
+
     def get_max(self):
         if self.root is None:
             return None
@@ -267,7 +283,75 @@ class AVLTreeList(object):
                 prev_node.setLeft(node)
             else:
                 self.get_predecessor(prev_node).setRight(node)
-        # fix the tree - update size and height of each node to root
+        return self.fix_the_tree(node)
+
+    def fix_the_tree(self, inserted_node):
+        rotations_count = 0
+        y = inserted_node.getParent()
+        while y is not None:
+            y_old_height = y.getHeight()
+            y.setHeight(max(y.getLeft().getHeight(), y.getRight().getHeight()) + 1)
+            balance_factor = y.getBalanceFactor()
+            if abs(balance_factor) < 2 and y.getHeight() == y_old_height:
+                break
+            elif abs(balance_factor) < 2 and y.getHeight() != y_old_height:
+                y = y.getParent()
+                continue
+            rotations_count += self.perform_rotation(y)
+            break
+        self.fix_nodes_size(inserted_node)
+        return rotations_count
+
+    def perform_rotation(self, bf_criminal):
+        if bf_criminal.getBalanceFactor() == 2:
+            if bf_criminal.getLeft().getBalanceFactor() == -1:
+                self.left_rotation(bf_criminal.getLeft())
+                self.right_rotation(bf_criminal)
+                return 2
+            self.right_rotation(bf_criminal)
+            return 1
+        if bf_criminal.getRight().getBalanceFactor() == -1:
+            self.left_rotation(bf_criminal)
+            return 1
+        self.right_rotation(bf_criminal.getRight())
+        self.left_rotation(bf_criminal)
+        return 2
+
+    def left_rotation(self, node_a):
+        node_b = node_a.getRight()
+        node_a_parent = node_a.getParent()
+        node_a.setRight(node_b.getLeft())
+        node_b.setLeft(node_a)
+        if node_a_parent.getRight() == node_a:
+           node_a_parent.setRight(node_b)
+        else:
+            node_a_parent.setLeft(node_b)
+
+        self.fix_nodes_height_and_size(node_a)
+        self.fix_nodes_height_and_size(node_b)
+
+    def right_rotation(self, node_b):
+        node_a = node_b.getLeft()
+        node_b_parent = node_b.getParent()
+        node_b.setLeft(node_a.getRight())
+        node_a.setRight(node_b)
+        if node_b_parent.getRight() == node_b:
+           node_b_parent.setRight(node_a)
+        else:
+            node_b_parent.setLeft(node_a)
+
+        self.fix_nodes_height_and_size(node_a)
+        self.fix_nodes_height_and_size(node_b)
+
+    def fix_nodes_height_and_size(self, node):
+        node.setSize(node.getLeft().getSize() + node.getRight().getSize() + 1)
+        node.setHeight(max(node.getLeft().getHeight(), node.getRight().getHeight()) + 1)
+
+#TODO - maybe change to static method?
+    def fix_nodes_size(self, inserted_node):
+        y = inserted_node.getParent()
+        while y is not None:
+            y.setSize(y.getSize() + 1)
 
     """deletes the i'th item in the list
 
