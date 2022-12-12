f6c9ea33c39e78493e4bba31438430c4f02d5f58
Elad <eladv@mail.tau.ac.il>
2022-12-12 14:58:22 +0200

    added tester.py

---
+++ tester.py
@@ -0,0 +1,13 @@
+import avl_template_new
+
+lst = avl_template_new.AVLTreeList()
+lst.insert(0, "a")
+lst.insert(1, "b")
+lst.insert(2, "c")
+print(lst)
+# lst.insert(3, "d")
+# print(lst)
+# lst.insert(4, "e")
+# print(lst)
+# lst.insert(5, "f")
+# print(lst)
