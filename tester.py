420c6d2667b3c42bf6e3c0aa038b41e9df14d881
Elad <eladv@mail.tau.ac.il>
2022-12-29 14:24:32 +0200

    changed tester

--- tester.py
+++ tester.py
@@ -1,33 +1,37 @@
+import collections
 import random
-
+import time
+import linked_list
 import avl_template_new
 
-# for i in range(1, 11):
-#     lst = avl_template_new.AVLTreeList()
-#     n = 1500 * (2 ** i)
-#     #n = 50 * i
-#     insert_count = 0
-#     delete_count = 0
-#     for j in range(n):
-#         random_position = random.randint(0, lst.length())
-#         insert_count += lst.insert(random_position, str(j))
-#     for j in range(n):
-#         random_position = random.randint(0, lst.length() - 1)
-#         delete_count += lst.delete(random_position)
-#     print("i = ", i, ", insert_count = ", insert_count, ", delete_count = ", delete_count)
-#
-lst = avl_template_new.AVLTreeList()
-lst.insert(0, None)
-lst.insert(0, "a")
-lst.insert(0, "b")
-lst.insert(0, "c")
-lst.insert(0, "d")
-lst.insert(0, None)
-lst.insert(0, None)
-lst.insert(0, None)
-lst.insert(0, None)
-print(lst.listToArray())
-print(lst.sort().listToArray())
+
+
+for i in range(1, 11):
+    lst = avl_template_new.AVLTreeList()
+    # lst = collections.deque()
+    # lst = list()
+    n = 1500 * i
+    insert_count = 0
+    start = time.time()
+    for j in range(n):
+        # random_position = random.randint(0, len(lst))
+        lst.insert(0, str(j))
+        # lst.append(str(j))
+    end = time.time()
+    print((end - start) / n)
+
+# lst = avl_template_new.AVLTreeList()
+# lst.insert(0, None)
+# lst.insert(0, "a")
+# lst.insert(0, "b")
+# lst.insert(0, "c")
+# lst.insert(0, "d")
+# lst.insert(0, None)
+# lst.insert(0, None)
+# lst.insert(0, None)
+# lst.insert(0, None)
+# print(lst.listToArray())
+# print(lst.sort().listToArray())
 #
 # lst2 = [None, None]
 # print(lst2)
